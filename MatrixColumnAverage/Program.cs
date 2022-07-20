1f9c7dfb2fe8d01211e8207e6827612273e0f638
ELENA <elenakand@icloud.com>
2022-07-20 23:39:08 +0300

    Среднее арифметическое столбцов матрицы

--- MatrixColumnAverage/Program.cs
+++ MatrixColumnAverage/Program.cs
@@ -7,3 +7,55 @@
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+int [,] InitMatrix(int m, int n)
+{
+    int [,] matrix = new int[m,n];
+    Random randomizer = new Random();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i,j] = randomizer.Next(1,10);
+            }
+        }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+} 
+void FindAverage(int[,] matrix)
+{
+    double[] result = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            result[j] += matrix[i, j];
+        }
+    Console.Write($"{result[j] / matrix.GetLength(0)} ");  
+    }
+}
+
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine($"Матрица размером {m}x{n}:");
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+FindAverage(matrix);
+Console.WriteLine();
